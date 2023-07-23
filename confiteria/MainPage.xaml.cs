dc00d0f044d35a1ece3553a38dd5cbe745a3e18f
MASTERALD <alexmeraz4567@gmail.com>
2023-07-22 18:42:49 -0600

    Pantalla de Confiteria

--- confiteria/MainPage.xaml.cs
+++ confiteria/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using confiteria.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -10,9 +11,33 @@ namespace confiteria
 {
     public partial class MainPage : ContentPage
     {
+        public List<Golosina> ConfiteriaList { get; set; }
+
         public MainPage()
         {
             InitializeComponent();
+
+            ConfiteriaList = new List<Golosina>
+        {
+            new Golosina
+            {
+                NombreProducto = "Combo 1",
+                // ImagenProducto = "confiteria.Resources.Images.Combo1.jpg" 
+                Descripcion = "Palomitas de Maiz + 2 Refrescos",
+                Precio = 120,
+               
+            },
+            new Golosina
+            {
+                NombreProducto = "Combo 2",
+                // ImagenProducto = "confiteria.Resources.Images.Combo2.jpg" 
+                Descripcion = "Palomitas de Maiz + 1 Refresco",
+                Precio = 95,
+               
+            },
+        };
+
+            BindingContext = this;
         }
     }
 }
